a98ea85d07fff13c02a5445a6dec302f84bfdd14
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-08-28 17:37:23 +0300

    Finished Directory sorter

--- foldershell.ps1
+++ foldershell.ps1
@@ -5,5 +5,6 @@ $sortedpath=Get-Childitem $inputpath
 foreach ($file in $sortedpath) {
     $filename=$file.FullName
     $extension=[IO.Path]::GetExtension($filename)
-    
+    mkdir "$inputpath\$extension\" -ErrorAction SilentlyContinue
+    Move-Item -Path "$filename" -Destination "$inputpath\$extension\"
 }
