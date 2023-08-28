4c4f5c1c35ca8be08afc13584b1aaa737ed14180
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-08-28 14:30:28 +0300

    Testing powershell atributes

--- foldershell.ps1
+++ foldershell.ps1
@@ -1,1 +1,8 @@
 Write-Host "Folder-Shell, Developed by MST Studios"
+Write-Host "Input the path you would like to be sorted:"
+$inputpath=Read-Host
+$sortedpath=Get-Childitem $inputpath
+foreach ($file in $sortedpath) {
+    $filename=$file.FullName
+    echo $filename
+}
