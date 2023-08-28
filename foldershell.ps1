458d90ebd298e63d3a2a8ac9d15d3a56dd2c07d2
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-08-28 18:20:15 +0300

    Added Intro and action choser

--- foldershell.ps1
+++ foldershell.ps1
@@ -1,10 +1,23 @@
-Write-Host "Folder-Shell, Developed by MST Studios"
-Write-Host "Input the path you would like to be sorted:"
-$inputpath=Read-Host
-$sortedpath=Get-Childitem $inputpath
-foreach ($file in $sortedpath) {
-    $filename=$file.FullName
-    $extension=[IO.Path]::GetExtension($filename)
-    mkdir "$inputpath\$extension\" -ErrorAction SilentlyContinue
-    Move-Item -Path "$filename" -Destination "$inputpath\$extension\"
+function Introduction {
+    Write-Host "Folder-Shell, Developed by MST Studios"
+    Write-Host "What would you like to do? (Enter the number of the respective action.)"
+    Write-Host "1-Sort Files in a Given directory"
+
+    $chosen_action=Read-Host "Action:"
+
+    if ($chosen_action -eq 1){SortFiles}
 }
+
+function SortFiles {
+    Write-Host "Input the path you would like to be sorted:"
+    $inputpath=Read-Host
+    $sortedpath=Get-Childitem $inputpath
+    foreach ($file in $sortedpath) {
+        $filename=$file.FullName
+        $extension=[IO.Path]::GetExtension($filename)
+        mkdir "$inputpath\$extension\" -ErrorAction SilentlyContinue
+        Move-Item -Path "$filename" -Destination "$inputpath\$extension\"
+    }    
+}
+
+Introduction
