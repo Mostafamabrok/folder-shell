2f7c6906ec187a67c096a479f37a1eaede456fef
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-08-29 21:19:50 +0300

    Corrected some introductory strings

--- foldershell.ps1
+++ foldershell.ps1
@@ -1,12 +1,12 @@
 function Introduction {
     #The introduction function. Introduces the program and asks the user what action to take.
     Write-Host "Folder-Shell, Developed by MST Studios`n"
-    Write-Host "What would you like to do? (Enter the number of the respective action.)"
-    Write-Host "1-Sort Files in a Given directory`n"
-    Write-Host "2-Move a File`n"
-    Write-Host "3-Change Saved Directories"
+    Write-Host "What would you like to do? (Enter the number of the respective action.)`n"
+    Write-Host "1-Sort Files in a Given directory"
+    Write-Host "2-Move a File"
+    Write-Host "3-Change Saved Directories`n"
 
-    $chosen_action=Read-Host "Action:"
+    $chosen_action=Read-Host "Action"
 
     if ($chosen_action -eq 1){SortFiles}
     if ($chosen_action -eq 2){SendFiles}
